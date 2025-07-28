a6b83e79b4d71498ce42433d99b591e1588f7748
Ngo Xuan Thiep <ngothiep2412@gmail.com>
2025-07-28 09:58:03 +0700

    feat: implement database models and accessors for problems, submissions, and users

--- internal/handlers/http/rpc/problem.go
+++ internal/handlers/http/rpc/problem.go
@@ -22,9 +22,8 @@ type ProblemSnippet struct {
 	ID                     uint64
 	DisplayName            string `validate:"alphanumunicode,min=1,max=256"`
 	Author                 User
-	TimeLimitInMillisecond uint64           `validate:"max=10000"`
-	MemoryLimitInByte      uint64           `validate:"max=8589934592"`
-	ExampleList            []ProblemExample `validate:"max=5"`
+	TimeLimitInMillisecond uint64 `validate:"max=10000"`
+	MemoryLimitInByte      uint64 `validate:"max=8589934592"`
 	CreatedTime            uint64
 	UpdatedTime            uint64
 }
